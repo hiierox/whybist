5d8fd842b2978ffd653138c82cd1ddde31316cb6
Kirill Batrakov <dl9ceb9@gmail.com>
2026-02-22 17:21:53 +0700

    AuthService added get_user_by_id + logging

--- services/auth/src/app/logic/auth_service.py
+++ services/auth/src/app/logic/auth_service.py
@@ -1,3 +1,5 @@
+import logging
+
 import bcrypt
 from pydantic import UUID4
 from sqlalchemy.exc import IntegrityError
@@ -19,6 +21,8 @@ from app.core.security import create_access_token, create_refresh_token
 from app.database.models import User
 from app.database.repository import UserRepository
 
+logger = logging.getLogger(__name__)
+
 
 class AuthService:
     def __init__(self, session: AsyncSession) -> None:
@@ -27,6 +31,13 @@ class AuthService:
     @staticmethod
     def _check_password(request_password: str, hashed_password: str) -> bool:
         return bcrypt.checkpw(request_password.encode(), hashed_password.encode())
+
+    async def get_user_by_id(self, identifier: UUID4) -> User | None:
+        user_repo = UserRepository(self.session)
+        user_data = await user_repo.get_user_by_id(identifier)
+        if not user_data:
+            raise UserNotFoundError('User not found')
+        return user_data
 
     async def register(self, request: RegisterRequest) -> User:
         salt = bcrypt.gensalt()
@@ -40,16 +51,19 @@ class AuthService:
                     username=request.username,
                     hashed_password=hashed_password,
                 )
+                logger.info('Successfully created User object')
                 await user_repo.create_user(user_data=user_data)
         except IntegrityError as e:
             raise UserAlreadyExistsError(
                 'User with this email or username already exists'
             ) from e
+        logger.info('Successfully created user')
         return user_data
 
     async def login(self, request: LoginRequest) -> Token:
         user_repo = UserRepository(self.session)
         user_data = await user_repo.get_user_by_email_or_username(request.identifier)
+        logger.info('Successfully got user by username|email')
         if not user_data:
             raise InvalidCredentialsError('Invalid username or password')
 
@@ -58,9 +72,10 @@ class AuthService:
         )
         if not is_password_correct:
             raise InvalidCredentialsError('Invalid username or password')
-
+        logger.info('Successfull passwords check')
         access_token = create_access_token(user_id=user_data.id)
         refresh_token = create_refresh_token(user_id=user_data.id)
+        logger.info('Successfully generated tokens')
         return Token(
             access_token=access_token, refresh_token=refresh_token, token_type='bearer'
         )
@@ -85,8 +100,10 @@ class AuthService:
             ).decode('utf-8')
 
             await user_repo.update_user(user_data, hashed_password=hashed_password)
+        logger.info('Successfully updated password')
         access_token = create_access_token(user_id=user_data.id)
         refresh_token = create_refresh_token(user_id=user_data.id)
+        logger.info('Successfully generated tokens')
         return Token(
             access_token=access_token, refresh_token=refresh_token, token_type='bearer'
         )
@@ -99,10 +116,12 @@ class AuthService:
             async with self.session.begin():
                 user_repo = UserRepository(self.session)
                 user_data = await user_repo.get_user_by_id(user_id)
+                logger.info('Successfully got user by id')
                 if not user_data:
                     raise UserNotFoundError('User not found')
                 if to_update:
                     await user_repo.update_user(user=user_data, **to_update)
+            logger.info('Successfully updated email/username')
             return user_data
         except IntegrityError as e:
             raise UserAlreadyExistsError(
