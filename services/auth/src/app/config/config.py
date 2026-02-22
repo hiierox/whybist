b977d72400b64c46df6bb867bb1414b28e2bc3bc
Kirill Batrakov <dl9ceb9@gmail.com>
2026-02-22 13:25:49 +0700

    config update

--- services/auth/src/app/config/config.py
+++ services/auth/src/app/config/config.py
@@ -4,6 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env')
     DATABASE_URL: str = 'postgresql+asyncpg://user:pass@host:port/db'
+    SECRET_KEY: str = ''
 
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_DAYS = 7
 
 settings = Settings()
